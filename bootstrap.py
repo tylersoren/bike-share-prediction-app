bf9f735187afc8428e59c548fe3fb370604b5236
Tyler Sorensen <tyler.sorensen@cellularsales.com>
2021-01-09 15:19:32 -0500

    moved code structure

--- bootstrap.py
+++ bootstrap.py
@@ -3,7 +3,7 @@ from tables import BikeData
 
 import os
 
-model_path = os.path.abspath(os.path.join(os.getcwd(), '../..', 'models/bike_share_v1.0' ))
+model_path = os.path.abspath(os.path.join(os.getcwd(),'models/bike_share_v1.0' ))
 
 def startup():
   model = BikeShareModel(model_path)
--- tables.py
+++ tables.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import os
 
-data_subdir = 'data\\prepared'
+data_subdir = 'data/prepared'
 weather_file = 'weather.csv'
 transaction_file = 'bike_transactions.csv'
 hourly_ride_file = 'hourly_rides.csv'
 
-data_dir = os.path.abspath(os.path.join(os.getcwd(), '../..', data_subdir))
+data_dir = os.path.abspath(os.path.join(os.getcwd(), data_subdir))
 class BikeData:
 
   def __init__(self):
